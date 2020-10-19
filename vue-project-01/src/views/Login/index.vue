176d353d927408059dfbd02bb75a39ccc44e18ac
Haocheng <yuanhaocheng910505@gmail.com>
2020-10-19 21:06:38 +1100

    create project setup router and UI

---
+++ vue-project-01/src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    Login Page
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
